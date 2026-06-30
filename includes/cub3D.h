de5db924951bc56af5bb18ca248e14b77c428b3d
julauren <249057325+Simeriash@users.noreply.github.com>
2026-06-30 17:40:40 +0200

    add input management

---
+++ includes/cub3D.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3D.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: julauren <julauren@student.42angouleme.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/30 16:43:17 by julauren          #+#    #+#             */
+/*   Updated: 2026/06/30 17:34:38 by julauren         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include "../libft/libft.h"
+# include <stdio.h>
+# include <stdlib.h>
+
+void	parsing(char *str);
+
+#endif
