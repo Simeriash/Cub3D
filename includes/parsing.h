eafe9f75561ee325431ae142bee285008f715d0a
julauren <249057325+Simeriash@users.noreply.github.com>
2026-07-09 17:37:28 +0200

    new struct

--- includes/parsing.h
+++ includes/parsing.h
@@ -6,7 +6,7 @@
 /*   By: julauren <julauren@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/30 17:42:38 by julauren          #+#    #+#             */
-/*   Updated: 2026/07/04 15:29:29 by julauren         ###   ########.fr       */
+/*   Updated: 2026/07/09 17:36:16 by julauren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,31 @@
 
 # include "cub3D.h"
 # include "../libft/libft.h"
-// # include <fcntl.h>
 # include <stdlib.h>
 
 typedef enum e_error
 {
-	MALLOC
+	MALLOC,
+	INPUT
 }	t_error;
 
+typedef struct	s_param
+{
+	t_data	*data;
+	char	*file;
+}	t_param;
 
-void	free_struct(t_data data, char **tab);
+void	check_data(t_param param, t_data *data);
+
+/*====================utils====================*/
+
+void	free_struct(t_data *data, char **map);
+int		empty_line(char *str, int start, int end);
+int		next_line(char *str, int start);
 
 /*====================error====================*/
 
 void	error_parsing(t_error error);
+void	free_error(char *str, t_param param, char **map, t_error error);
 
 #endif
