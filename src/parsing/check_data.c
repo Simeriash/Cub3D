4d8d31ebe351f03bb9734a08d2e964da72d58849
julauren <249057325+Simeriash@users.noreply.github.com>
2026-07-09 17:38:09 +0200

    parsing

---
+++ src/parsing/check_data.c
@@ -0,0 +1,136 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_data.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: julauren <julauren@student.42angouleme.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/09 11:46:38 by julauren          #+#    #+#             */
+/*   Updated: 2026/07/09 17:23:26 by julauren         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/parsing.h"
+
+static void	add_str(char **dest, char *src, int i, t_param param)
+{
+	int	end;
+
+	i += 2;
+	if (!ft_isspace(src[i]))
+	{
+		free(src);
+		free_error(NULL, param, NULL, INPUT);
+	}
+	while (src[i] && ft_isspace(src[i]))
+		i++;
+	end = i;
+	while (src[i] && !ft_isspace(src[end]))
+		end++;
+	*dest = ft_substr(src, i, end - i);
+	free(src);
+	if (!dest)
+		free_error(NULL, param, NULL, MALLOC);
+}
+
+static int	next_number(char *src, int *i, t_param param)
+{
+	int		nb;
+	int		j;
+	char	*str;
+
+	j = *i;
+	while (src[*i] && !(ft_isspace(src[*i]) || src[*i] == ','))
+		(*i)++;
+	str = ft_substr(src, j, *i - j);
+	if (!str)
+		free_error(src, param, NULL, MALLOC);
+	j = 0;
+	// printf("%s\n", str);
+	while (str[j] && ft_isdigit(str[j]))
+		j++;
+	if (str[j] != '\0' || ft_atoi(str, &nb))
+	{
+		free(str);
+		free_error(src, param, NULL, INPUT);
+	}
+	free(str);
+	while (src[*i] && (ft_isspace(src[*i]) || src[*i] == ','))
+		(*i)++;
+	return (nb);
+}
+
+static void	add_int(int **dest, char *src, int i, t_param param)
+{
+	int	n;
+
+	if (!ft_isspace(src[i + 1]))
+	{
+		free(src);
+		free_error(NULL, param, NULL, INPUT);
+	}
+	*dest = malloc(sizeof(**dest) * 3);
+	if (!(*dest))
+		free_error(src, param, NULL, MALLOC);
+	i++;
+	while (src[i] && ft_isspace(src[i]))
+		i++;
+	n = 0;
+	while (n < 3)
+		(*dest)[n++] = next_number(src, &i, param);
+	free(src);
+}
+
+static int	add_data(char *str, t_data *data, t_param param)
+{
+	int	i;
+
+	i = 0;
+	while (ft_isspace(str[i]))
+		i++;
+	if (!data->NO && !ft_memcmp(&str[i], "NO", 2))
+		add_str(&data->NO, str, i, param);
+	else if (!data->SO && !ft_memcmp(&str[i], "SO", 2))
+		add_str(&data->SO, str, i, param);
+	else if (!data->WE && !ft_memcmp(&str[i], "WE", 2))
+		add_str(&data->WE, str, i, param);
+	else if (!data->EA && !ft_memcmp(&str[i], "EA", 2))
+		add_str(&data->EA, str, i, param);
+	else if (!data->F && !ft_memcmp(&str[i], "F", 1))
+		add_int(&data->F, str, i, param);
+	else if (!data->C && !ft_memcmp(&str[i], "C", 1))
+		add_int(&data->C, str, i, param);
+	else
+	{
+		free(str);
+		return (1);
+	}
+	return (0);
+}
+
+void	check_data(t_param param, t_data *data)
+{
+	int		start;
+	int		end;
+	char	*str;
+
+	param.data = data;
+	start = 0;
+	while (1)
+	{
+		end = next_line(param.file, start);
+		// printf("start: %i\tend: %i\n", start, end);
+		if (end == -1)
+			break ;
+		if (!empty_line(param.file, start, end))
+		{
+			str = ft_substr(param.file, start, end - start + 1);
+			// printf("%s\n", str);
+			if (!str)
+				free_error(NULL, param, NULL, MALLOC);
+			if (add_data(str, data, param))
+				break ;
+		}
+		start = end + 1;
+	}
+}
--- src/parsing/parsing_utils.c
+++ src/parsing/parsing_utils.c
@@ -6,33 +6,70 @@
 /*   By: julauren <julauren@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/04 11:18:26 by julauren          #+#    #+#             */
-/*   Updated: 2026/07/04 15:12:45 by julauren         ###   ########.fr       */
+/*   Updated: 2026/07/09 17:24:08 by julauren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/parsing.h"
 
-void	free_struct(t_data data, char **tab)
+void	free_struct(t_data *data, char **map)
 {
 	int	i;
 
-	if (data.NO)
-		free(data.NO);
-	if (data.SO)
-		free(data.SO);
-	if (data.WE)
-		free(data.WE);
-	if (data.EA)
-		free(data.EA);
-	if (data.F)
-		free(data.F);
-	if (data.C)
-		free(data.C);
-	if (tab)
+	if (data->NO)
+		free(data->NO);
+	if (data->SO)
+		free(data->SO);
+	if (data->WE)
+		free(data->WE);
+	if (data->EA)
+		free(data->EA);
+	if (data->F)
+		free(data->F);
+	if (data->C)
+		free(data->C);
+	if (map)
 	{
 		i = 0;
-		while (tab[i])
-			free(tab[i++]);
-		free(tab);
+		while (map[i])
+			free(map[i++]);
+		free(map);
 	}
+}
+
+int	empty_line(char *str, int start, int end)
+{
+	int	i;
+
+	if (start > end)
+		return (0);
+	i = start;
+	while (str[i] && ft_isspace(str[i]) && i <= end)
+		i++;
+	if (str[i] == '\0' || i == end + 1)
+		return (1);
+	return (0);
+}
+
+int	next_line(char *str, int start)
+{
+	int	end;
+
+	end = start;
+	while (str[end])
+	{
+		if (str[end] == '\n')
+			return (end);
+		end++;
+	}
+	return (-1);
+}
+
+void	free_error(char *str, t_param param, char **map, t_error error)
+{
+	if (str)
+		free(str);
+	free_struct(param.data, map);
+	free(param.file);
+	error_parsing(error);
 }
