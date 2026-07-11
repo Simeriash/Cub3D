296888691f8c46132ce8d696b4402c3723ada255
julauren <249057325+Simeriash@users.noreply.github.com>
2026-07-11 13:44:03 +0200

    map become rectangular

--- src/parsing/clear_map.c
+++ src/parsing/clear_map.c
@@ -6,7 +6,7 @@
 /*   By: julauren <julauren@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/10 14:16:39 by julauren          #+#    #+#             */
-/*   Updated: 2026/07/10 15:50:27 by julauren         ###   ########.fr       */
+/*   Updated: 2026/07/11 11:39:14 by julauren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,20 +22,38 @@ int	tab_len(char **tab)
 	return (i);
 }
 
-static char	*in_line(char *str, int n)
+static int	max_strlen(char **tab, int n)
+{
+	int	i;
+	int	len;
+	int	tmp;
+
+	i = 0;
+	tmp = 0;
+	while (tab[i])
+	{
+		len = end_of_file(tab[i], n);
+		if (len > tmp)
+			tmp = len;
+		i++;
+	}
+	return (tmp);
+}
+
+static char	*in_line(char *str, int n, int len)
 {
 	char	*line;
-	int		end;
 	int		i;
 
-	end = end_of_file(str, 0);
-	line = malloc(sizeof(*line) * (end - n + 2));
+	line = malloc(sizeof(*line) * (len - n + 2));
 	if (!line)
 		return (NULL);
 	i = 0;
-	end -= n;
-	while (i <= end)
+	len -= n;
+	while (str[n])
 		line[i++] = str[n++];
+	while (i <= len)
+		line[i++] = ' ';
 	line[i] = '\0';
 	return (line);
 }
@@ -43,17 +61,19 @@ static char	*in_line(char *str, int n)
 char	**clear_map(char **tab, int n, t_param param)
 {
 	char	**map;
-	int		len;
+	int		size;
 	int		i;
+	int		len;
 
-	len = tab_len(tab);
-	map = malloc(sizeof(*map) * (len + 1));
+	size = tab_len(tab);
+	len = max_strlen(tab, n);
+	map = malloc(sizeof(*map) * (size + 1));
 	if (!map)
 		free_error(NULL, param, tab, MALLOC);
 	i = 0;
-	while (i < len)
+	while (i < size)
 	{
-		map[i] = in_line(tab[i], n);
+		map[i] = in_line(tab[i], n, len);
 		if (!map[i])
 		{
 			free_tab(map);
